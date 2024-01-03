d1c7819b7beb8964f8d0b37a4bd2a20018b43add
martprenger <mart.prenger@gmail.com>
2024-01-03 22:22:31 +0100

    start

---
+++ src/Main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by pyro on 1/3/24.
+//
+
+#include <iostream>
+
+#include "Main.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ src/Main.h
@@ -0,0 +1,14 @@
+//
+// Created by pyro on 1/3/24.
+//
+
+#ifndef ALGORITHMS_MAIN_H
+#define ALGORITHMS_MAIN_H
+
+
+class Main {
+
+};
+
+
+#endif //ALGORITHMS_MAIN_H
